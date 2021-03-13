82a4bb59c42357c83dfb65135c47fe53c7eb846b
tatuphi <tatuphi@gmail.com>
2021-03-13 18:07:58 +0700

    Fix error get response from url

--- app/src/main/java/com/example/androidtv/MyBroadcastReceiver.java
+++ app/src/main/java/com/example/androidtv/MyBroadcastReceiver.java
@@ -13,12 +13,10 @@ import android.os.AsyncTask;
 import android.os.Handler;
 import android.util.DisplayMetrics;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.io.IOException;
 import java.util.Objects;
@@ -28,9 +26,12 @@ import okhttp3.Response;
 
 public class MyBroadcastReceiver extends BroadcastReceiver {
     public static final String NOTIFICATION_CHANNEL_ID = "10001" ;
-    public static String NOTIFICATION_ID = "notification-id";
+    public static String NOTIFICATION_ID = "notification_id";
     public static String NOTIFICATION = "notification";
-
+    public static String FOOD_WEIGHT = "food_weight";
+    public static String EATING_TIME = "eating_time";
+    public static String EATING_SPEED = "eating_speed";
+    public static String START_TIME = "start_time";
 
     int widthToast = 0, heightToast = 0;
     int percent = 20;
@@ -44,114 +45,123 @@ public class MyBroadcastReceiver extends BroadcastReceiver {
     @SuppressLint("StaticFieldLeak")
     @Override
     public void onReceive(final Context context, Intent intent) {
-        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        Notification notification = intent.getParcelableExtra(NOTIFICATION);
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            int importance = NotificationManager.IMPORTANCE_HIGH;
-            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "NOTIFICATION_CHANNEL_NAME", importance);
+        if(Intent.ACTION_SHUTDOWN.equals(intent.getAction()))
+        {
+            Log.d("abc", "System shutting down");
+            context.stopService(new Intent(context, MyBroadcastReceiver.class));
+        } else {
+            Log.d("abc", "type action: " + intent.getAction());
+            NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            Notification notification = intent.getParcelableExtra(NOTIFICATION);
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                int importance = NotificationManager.IMPORTANCE_HIGH;
+                NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "NOTIFICATION_CHANNEL_NAME", importance);
+                assert notificationManager != null;
+                notificationManager.createNotificationChannel(notificationChannel);
+            }
+            Log.d("abc", " here ");
+
+            int id = intent.getIntExtra(NOTIFICATION_ID, 0);
+            int eatingTime = intent.getIntExtra(EATING_TIME, 0);
+            final int eatingSpeed = intent.getIntExtra(EATING_SPEED, 0);
+            final int startWeight = intent.getIntExtra(FOOD_WEIGHT, 0);
+            final long startTime = intent.getLongExtra(START_TIME, 0);
+            final long endTime = startTime + eatingTime*60*1000;
+
             assert notificationManager != null;
-            notificationManager.createNotificationChannel(notificationChannel);
-        }
-        Log.d("abc", " here ");
-        int id = intent.getIntExtra(NOTIFICATION_ID, 0);
-        assert notificationManager != null;
-        notificationManager.notify(id, notification);
+            notificationManager.notify(id, notification);
 
-        DisplayMetrics displayMetrics = new DisplayMetrics();
-        WindowManager windowManager = (WindowManager) context
-                .getSystemService(Context.WINDOW_SERVICE);
-        windowManager.getDefaultDisplay().getMetrics(displayMetrics);
-        widthScreen = displayMetrics.widthPixels;
-        heightScreen = displayMetrics.heightPixels;
-        Log.d("abc", "Width screen " + widthScreen);
-        Log.d("abc", "height screen " + heightScreen);
+            DisplayMetrics displayMetrics = new DisplayMetrics();
+            WindowManager windowManager = (WindowManager) context
+                    .getSystemService(Context.WINDOW_SERVICE);
+            windowManager.getDefaultDisplay().getMetrics(displayMetrics);
+            widthScreen = displayMetrics.widthPixels;
+            heightScreen = displayMetrics.heightPixels;
+            Log.d("abc", "Width screen " + widthScreen);
+            Log.d("abc", "height screen " + heightScreen);
 
-        // Build Notification with Notification Manager
+            // Build Notification with Notification Manager
+            new AsyncTask<String, String, Integer>() {
+                @Override
+                protected Integer doInBackground(String... strings) {
+                    TestMain example = new TestMain();
+                    String getResponse = "", number = null;
+                    try {
+                    getResponse = example.doGetRequest("http://172.16.6.220/loadcell");
 
-        new AsyncTask<String, String, Integer>() {
-            @Override
-            protected Integer doInBackground(String... strings) {
-                TestMain example = new TestMain();
-                String getResponse, number = null;
-                try {
-                    getResponse = example.doGetRequest("http://www.randomnumberapi.com/api/v1.0/random?min=0&max=100");
-                    Log.d("abc", "getResponse: " + getResponse);
-                    number = getResponse.substring(getResponse.indexOf("[") + 1, getResponse.indexOf("]"));
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                return Integer.parseInt(number);
-            }
-
-            @Override
-            protected void onPostExecute(Integer s) {
-                super.onPostExecute(s);
-                LayoutInflater inflater = LayoutInflater.from(context);
-                View layout = inflater.inflate(R.layout.custom_toast, null);
-                TextView text = (TextView) layout.findViewById(R.id.text);
-                text.setText("This is a custom toast");
-                if (s > 100) {
-                    percent = 100;
-                } else {
-                    percent = s;
-                }
-                text.measure(0, 0);
-                widthToast = (Integer) (widthScreen * percent / 100);
-                Log.d("abc", "width toast " + widthToast);
-                heightToast = (Integer) (heightScreen * percent / 100);
-                Log.d("abc", "height toast " + heightToast);
-
-                text.setWidth(widthToast);
-                text.setHeight(heightToast);
-
-
-                Toast toast = new Toast(context);
-                toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
-                toast.setDuration(Toast.LENGTH_LONG);
-                toast.setView(layout);
-//                toast.show();
-
-                Toast toast1 = new Toast(context);
-                toast1.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
-                toast1.setDuration(Toast.LENGTH_LONG);
-                toast1.setView(layout);
-//                toast1.show();
-
-                Toast toast2 = new Toast(context);
-                toast2.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
-                toast2.setDuration(Toast.LENGTH_LONG);
-                toast2.setView(layout);
-//                toast2.show();
-                builder = new AlertDialog.Builder(context);
-                builder.setCancelable(false);
-                builder.setPositiveButton("Cancel", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
+//                        getResponse = example.doGetRequest("https://api64.ipify.org/?format=json");
+                        Log.d("abc", "getResponse: " + getResponse);
+//                    number = getResponse.substring(getResponse.indexOf("[") + 1, getResponse.indexOf("]"));
+                        Log.d("abc", "getResponse: " + getResponse);
+                    } catch (IOException e) {
+                        e.printStackTrace();
                     }
-                });
-                alertDialog = builder.create();
-                alertDialog.setView(layout);
-                if (alertDialog.getWindow() != null) {
-                    int type;
-                    type = WindowManager.LayoutParams.TYPE_TOAST;
-                    alertDialog.getWindow().setType(type);
+//                return Integer.parseInt(getResponse);
+                    int curWeight = Integer.parseInt(getResponse);
+                    int result = getPercent(startTime, endTime, curWeight, startWeight, eatingSpeed);
+                    return result;
                 }
-                alertDialog.show();
-                Log.d("abc", "Result is main: " + s);
-                Runnable dismissDialog = new Runnable() {
-                    @Override
-                    public void run() {
-                        if (alertDialog != null) {
-                            alertDialog.dismiss();
+
+                @Override
+                protected void onPostExecute(Integer s) {
+                    super.onPostExecute(s);
+                    LayoutInflater inflater = LayoutInflater.from(context);
+                    View layout = inflater.inflate(R.layout.custom_toast, null);
+                    TextView text = (TextView) layout.findViewById(R.id.text);
+                    text.setText("This is a custom toast");
+                    if (s > 100) {
+                        percent = 100;
+                    } else {
+                        percent = s;
+                    }
+
+                    text.measure(0, 0);
+                    widthToast = (Integer) (widthScreen * percent / 100);
+                    Log.d("abc", "width toast " + widthToast);
+                    heightToast = (Integer) (heightScreen * percent / 100);
+                    Log.d("abc", "height toast " + heightToast);
+
+                    text.setWidth(widthToast);
+                    text.setHeight(heightToast);
+
+                    builder = new AlertDialog.Builder(context);
+                    builder.setCancelable(false);
+                    builder.setPositiveButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
                         }
+                    });
+                    alertDialog = builder.create();
+                    alertDialog.setView(layout);
+                    if (alertDialog.getWindow() != null) {
+                        int type;
+                        type = WindowManager.LayoutParams.TYPE_TOAST;
+                        alertDialog.getWindow().setType(type);
                     }
-                };
-                new Handler().postDelayed(dismissDialog, Constants.DELAY_TIMER);
-            }
-        }.execute();
 
+                    if(percent > 0){
+                        alertDialog.show();
+                    } else {
+                        alertDialog.dismiss();
+                    }
+
+                    Log.d("abc", "Result is main: " + s);
+                    Runnable dismissDialog = new Runnable() {
+                        @Override
+                        public void run() {
+                            if (alertDialog != null) {
+                                alertDialog.dismiss();
+                            }
+                        }
+                    };
+                    new Handler().postDelayed(dismissDialog, Constants.DELAY_TIMER);
+                }
+            }.execute();
+        }
     }
+
+
 
     public static class TestMain {
         OkHttpClient client = new OkHttpClient();
@@ -160,16 +170,25 @@ public class MyBroadcastReceiver extends BroadcastReceiver {
         String doGetRequest(String url) throws IOException {
             okhttp3.Request request = new okhttp3.Request.Builder()
                     .url(url)
-                    .addHeader("Accept", "application/text")
-                    .addHeader("Content-Type", "application/text")
-
-//                    .addHeader("Accept", "application/json")
-//                    .addHeader("Content-Type", "application/json")
-
+                    .addHeader("Accept", "application/json")
+                    .addHeader("Content-Type", "text/html")
                     .build();
-
             Response response = client.newCall(request).execute();
-            return Objects.requireNonNull(response.body()).string();
+            String finalResult = Objects.requireNonNull(response.body()).string();
+
+            Log.d("abc", "the first: " + finalResult);
+            return finalResult;
         }
+    }
+
+    int getPercent(long startTime, long endTime, int curWeight, int startWeight, int speed) {
+        int percentResult = 0;
+        long curTime = System.currentTimeMillis();
+        int timeInMinutes = (int) (curTime - startTime) / (1000 * 60);
+        int idealWeight = startWeight - (timeInMinutes * speed);
+        if (curWeight - idealWeight > 0) {
+            percentResult = (int) (curWeight - idealWeight) * 100 / speed;
+        }
+        return percentResult;
     }
 }
--- app/src/main/java/com/example/androidtv/activity/MainActivity.java
+++ app/src/main/java/com/example/androidtv/activity/MainActivity.java
@@ -2,11 +2,18 @@ package com.example.androidtv.activity;
 
 import android.annotation.SuppressLint;
 import android.app.AlarmManager;
+import android.app.AlertDialog;
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -17,32 +24,98 @@ import com.example.androidtv.MyBroadcastReceiver;
 import com.example.androidtv.R;
 
 public class MainActivity extends FragmentActivity {
-    public static final String NOTIFICATION_CHANNEL_ID = "10001" ;
-    private final static String default_notification_channel_id = "default" ;
+    public static final String NOTIFICATION_CHANNEL_ID = "10001";
+    private final static String default_notification_channel_id = "default";
+    int foodWeight = 0;
+    int time = 0;
+
+    EditText edtEatingTime;
+    Button btnEatingTime, btnScanWifi;
+
     @SuppressLint("StaticFieldLeak")
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        scheduleNotification(getNotification( "this is test notification")) ;
+        btnEatingTime = findViewById(R.id.btn_eating_time);
+        btnScanWifi = findViewById(R.id.btn_scan_wifi);
+
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View layout = inflater.inflate(R.layout.custom_dialog, null);
+        edtEatingTime = layout.findViewById(R.id.edt_time);
+
+//        fact: food get from url at start;
+        foodWeight = 300;
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(false);
+        builder.setView(layout);
+        builder.setPositiveButton("Start", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+//                dialog.dismiss();
+//                condition = check connection between tv and object, weight > 50 gram
+                if (!edtEatingTime.getText().toString().equals("")) {
+                    time = Integer.parseInt(edtEatingTime.getText().toString());
+                }
+                if (foodWeight > 50 && time > 0) {
+                    scheduleNotification(getNotification("this is test notification"));
+                } else {
+                    Toast.makeText(MainActivity.this, "Please put your food on", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+        btnScanWifi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("abc", "list wifi scanned here");
+            }
+        });
+        btnEatingTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                builder.show();
+            }
+        });
+//        AlertDialog alertDialog = builder.create();
+//        alertDialog.setView(layout);
+//        if (alertDialog.getWindow() != null) {
+//            int type;
+//            type = WindowManager.LayoutParams.TYPE_TOAST;
+//            alertDialog.getWindow().setType(type);
+//        }
+//        alertDialog.show();
+
+//        scheduleNotification(getNotification( "this is test notification")) ;
     }
-    private void scheduleNotification (Notification notification) {
-        Intent notificationIntent = new Intent(this, MyBroadcastReceiver. class ) ;
-        notificationIntent.putExtra(MyBroadcastReceiver. NOTIFICATION_ID , 1 ) ;
-        notificationIntent.putExtra(MyBroadcastReceiver. NOTIFICATION , notification) ;
-        PendingIntent pendingIntent = PendingIntent. getBroadcast ( this, 0 , notificationIntent , PendingIntent. FLAG_UPDATE_CURRENT ) ;
-        AlarmManager alarmManager = (AlarmManager) getSystemService(Context. ALARM_SERVICE ) ;
+
+    private void scheduleNotification(Notification notification) {
+        Intent notificationIntent = new Intent(this, MyBroadcastReceiver.class);
+        int eatingTime = Integer.parseInt(edtEatingTime.getText().toString());
+        int eatingSpeed = 0;
+        if(eatingTime>0){
+            eatingSpeed = (Integer) foodWeight/eatingTime;
+        }
+        notificationIntent.putExtra(MyBroadcastReceiver.NOTIFICATION_ID, 1);
+        notificationIntent.putExtra(MyBroadcastReceiver.NOTIFICATION, notification);
+        notificationIntent.putExtra(MyBroadcastReceiver.EATING_TIME, eatingTime );
+        notificationIntent.putExtra(MyBroadcastReceiver.START_TIME, System.currentTimeMillis());
+        notificationIntent.putExtra(MyBroadcastReceiver.FOOD_WEIGHT, foodWeight);
+        notificationIntent.putExtra(MyBroadcastReceiver.EATING_SPEED, eatingSpeed);
+
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
         assert alarmManager != null;
         alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), 60000, pendingIntent);
         Toast.makeText(getApplicationContext(), "Alarm will set in 60 seconds", Toast.LENGTH_LONG).show();
     }
-    private Notification getNotification (String content) {
-        NotificationCompat.Builder builder = new NotificationCompat.Builder( this, default_notification_channel_id ) ;
-        builder.setContentTitle( "Scheduled Notification" ) ;
-        builder.setContentText(content) ;
-        builder.setSmallIcon(R.drawable. ic_launcher_foreground ) ;
-        builder.setAutoCancel( true ) ;
-        builder.setChannelId( NOTIFICATION_CHANNEL_ID ) ;
-        return builder.build() ;
+
+    private Notification getNotification(String content) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, default_notification_channel_id);
+        builder.setContentTitle("Scheduled Notification");
+        builder.setContentText(content);
+        builder.setSmallIcon(R.drawable.ic_launcher_foreground);
+        builder.setAutoCancel(true);
+        builder.setChannelId(NOTIFICATION_CHANNEL_ID);
+        return builder.build();
     }
 }
